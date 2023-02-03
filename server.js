b453bd3345fc73480c963d346201becd08ce5df6
Big Trees <traveye@protonmail.com>
2023-02-02 16:59:22 -0800

    adding switch case for prompt

--- server.js
+++ server.js
@@ -31,11 +31,47 @@ async function init() {
         "Update Employee Role",
         "View All Roles",
         "Add Role",
-        "View All Departements",
+        "View All Departments",
         "Add Department",
       ],
     },
   ]);
-  console.log(userInput);
+
+  
+  switch(userInput.userChoice) {
+    case "View All Employees": 
+      console.log('viewing emps');
+      init();
+      break;
+    case "Add Employee":
+      console.log('adding emp');
+      init();
+      break;
+    case "Update Employee Role":
+      console.log('updating role');
+      init();
+      break;
+    case "View All Roles":
+      console.log('viewing roles');
+      init();
+      break;
+    case "Add Role":
+      console.log('adding role');
+      init();
+      break;
+    case "View All Departments":
+      console.log('vad');
+      init();
+      break;
+    case "Add Department":
+      console.log("adding D");
+      init();
+      break;
+    default:
+      console.log('something went wrong');
+      init();
+      break;
+
+  }
 }
 init();
